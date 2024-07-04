8c1dd21866d6b4223080a48e6656d407ea77be78
iqfarutrusted <muhammadiqfaru16@gmail.com>
2024-07-04 12:46:28 +0700

    Update 6

--- application/controllers/Katalog.php
+++ application/controllers/Katalog.php
@@ -3,12 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Katalog extends CI_Controller
 {
-
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('m_produk');
-	}
+	// ... your constructor ...
 
 	public function index()
 	{
@@ -18,16 +13,18 @@ class Katalog extends CI_Controller
 		$this->load->view('templates/footer');
 	}
 
-	public function produk($id_produk)
+	public function produk($id_produk = null)
 	{
-		$data['product'] = $this->m_produk->get_data_by_id($id_produk);
+		if ($id_produk !== null) {
+			$data['product'] = $this->m_produk->get_data_by_id($id_produk);
 
-		if (!$data['product']) {
-			show_404();
+			if (!$data['product']) {
+				show_404();
+			}
 		}
 
 		$this->load->view('templates/header');
-		$this->load->view('katalog/produk', $data);
+		$this->load->view('katalog/produk', $data); // Load product view always
 		$this->load->view('templates/footer');
 	}
 }
