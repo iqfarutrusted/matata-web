5cb11d7d99b2988e1ee43a05664f12e33d09f469
iqfarutrusted <muhammadiqfaru16@gmail.com>
2024-07-04 09:48:10 +0700

    Update M_produk.php

--- application/models/M_produk.php
+++ application/models/M_produk.php
@@ -4,11 +4,16 @@ defined('BASEPATH') or exit('no direct script access allowed');
 class M_Produk extends CI_Model
 {
 
-    public function tampil_data($id_produk)
+    public function get_data($id_produk)
     {
         $this->db->where('id_produk', $id_produk);
         $query = $this->db->get('produk');
         return $query->row();
+    }
+
+    public function tampil_data()
+    {
+        return $this->db->get('produk');
     }
 
     public function get_data_by_id($id_produk)
